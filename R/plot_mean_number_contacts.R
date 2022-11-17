0302b710883d4d966d813db64367fcadb8957b62
lwillem <lander.willem@uantwerpen.be>
2022-11-17 15:30:44 +0100

    Made sure that the age groups are shown in the mean number of contacts plot.

--- R/plot_mean_number_contacts.R
+++ R/plot_mean_number_contacts.R
@@ -3,16 +3,19 @@
 # 
 # => PLOT THE AVERAGE NUMBER OF SOCIAL CONTACTS BY AGE
 #
-#  Copyright 2020, SIMID, UNIVERSITY OF ANTWERP & HASSELT UNIVERSITY
+#  Copyright 2022, SIMID, UNIVERSITY OF ANTWERP & HASSELT UNIVERSITY
 #___________________________________________________________________________
 
 
-#mij <- contact_matrix(polymod, countries = "United Kingdom", age.limits = c(0, 1, 5, 15))$matrix
+# mij <- contact_matrix(polymod, countries = "United Kingdom", age.limits = c(0, 1, 5, 15))$matrix;scale_max=10
 #mij <- matrix_out$matrix
 plot_mean_number_contacts <- function(mij,scale_max=10){
   if(all(is.na(mij))){
     return(NA)
   }
+  
+  # make sure the age-groups are added to the figure
+  rownames(mij) <- colnames(mij)
   
   avg_contacts_age <- rowSums(mij)
   bplt <- barplot(avg_contacts_age,

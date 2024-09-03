a836bf6918f2e9ca3d83e9f12395b08cbc00fa34
lwillem <lander.willem@uantwerpen.be>
2024-09-03 08:50:20 +0200

    Updated the socrates_local script to enable the latest (dev) version.

--- R/modules/socrates_local.R
+++ R/modules/socrates_local.R
@@ -1,12 +1,12 @@
 #___________________________________________________________________________
 # This file is part of the SOcial Contact RATES (SOCRATES) modelling project
 # 
-# Run a remote SOCRATES UI
+# Run a local SOCRATES UI
 #
 # Copyright 2024, SIMID
 #___________________________________________________________________________
 
-# load package(s)
+# load packages
 library('shiny')
 library('socialmixr')
 library('countrycode')
@@ -22,12 +22,20 @@ library('curl')
 library('XML')
 library('rsconnect')
 
+# PUBLIC VERSION ----
+
 # option 1: run public UI via GitHub ----
 runGitHub('socrates_rshiny','lwillem')
 
 # option 2: run public UI via URL (of GitHub repo) ----
 runUrl('https://github.com/lwillem/socrates_rshiny/archive/master.tar.gz')
 
+
+# LATEST VERSION ----
+
 # option 3: run latest version
 runGitHub('socrates_rshiny','lwillem',ref = 'dev')
+
+# option 4: run latest CoMix version
+runGitHub('socrates_rshiny','lwillem',ref = 'dev_comix')
 
